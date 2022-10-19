cd06cb82cde3e951d0fdd7084a4f5b2e4bbe052d
Sandeep <sandeep@opstree.com>
2022-10-19 10:57:23 +0530

    Added code for dependency check

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ source functions.sh
 logInfoMessage "I'll scan the code for dependencies available at [$WORKSPACE] and have mounted at [$CODEBASE_DIR]"
 sleep  $SLEEP_DURATION
 cd  $WORKSPACE/${CODEBASE_DIR}
+
+COMPONENT_NAME=`getComponentName`
+/usr/share/dependency-check/bin/dependency-check.sh --scan $WORKSPACE/${CODEBASE_DIR} --project "${COMPONENT_NAME}"
